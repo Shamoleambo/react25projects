6be1de1c50c264bbac4ec7156fb6f47a652f5620
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-03 14:06:26 -0300

    make the star rating component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
   return (
     <div className='App'>
-      <RandomColor />
+      <StarRating  numberOfStars={10} />
     </div>
   )
 }
